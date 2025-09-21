fcb311df2ad3e2bdc5eec55d0e655ac61cf0dad4
pipes <144916002+pipesSomeAndSSam@users.noreply.github.com>
2025-09-22 00:15:37 +0800

    Added overview component design

--- app/components/Overview.tsx
+++ app/components/Overview.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 function Overview() {
   return (
-    <div>
-      <div>Overview</div>
+    <div className="overview-card">
+      <div className="overview-items">
+        <div className="overview-content">
+          <div>
+            <Label htmlFor="amount">Tip Amoint</Label>
+            <p className="person-text">/ person</p>
+          </div>
+          <p className="amount-text">$0.00</p>
+        </div>
+        <div className="overview-content">
+          <div>
+            <Label htmlFor="total">Total</Label>
+            <p className="person-text">/ person</p>
+          </div>
+          <p className="amount-text">$0.00</p>
+        </div>
+        <Button className="bg-[#26c2ad] text-[#045456] sm:mt-34 sm:px-20 flex">
+          RESET
+        </Button>
+      </div>
     </div>
   );
 }
